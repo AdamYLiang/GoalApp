638750ea227a019824e212c0c4a8d7a620f36ccb
AdamYLiang <ayl292@nyu.edu>
2018-12-21 15:36:21 -0500

    Complete User model

---
+++ app/models/user.rb
@@ -0,0 +1,59 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+  #F ind by credentials
+  #G enerate session token
+  #R eset session token
+  #I s password?
+  #P assword=
+  #E nsure session token 
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.update!(session_token: self.class.generate_session_token)
+    @session_token 
+  end
+
+  def is_password?(value)
+    decrypt_pass = BCrypt::Password.new(self.password_digest)
+    decrypt_pass.is_password?(password)
+  end
+
+  def password=(value)
+    @password = value
+    self.password_digest = BCrypt::Password.create(@password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_length_of(:password).is_at_least(6) }
+  it { should have_many(:goals) }
+
+  describe "uniqueness" do
+    before :each do
+      create(:user)
+    end
+    it { should validate_uniqueness_of(:session_token) }
+    it { should validate_uniqueness_of(:username) }
+  end
+
+  describe "is_password?" do
+    it 'blahblah'
+  end
+
+  describe "find_by_credentials?" do
+    it 'blahblah'
+  end
+
+  describe "reset_session_token!" do
+    it 'blahblah'
+  end
+
+
+end
